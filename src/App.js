c39ec7b614fe017f24070a1bd691963f512dd6b4
millidavids <david.a.yurek@gmail.com>
2017-07-05 21:42:21 -0400

    state management and rendering ui with external data

--- src/App.js
+++ src/App.js
@@ -1,13 +1,33 @@
-import React, { Component } from 'react';
+import React, { Component } from 'react'
+import ListContacts from './ListContacts'
+import * as ContactsAPI from './utils/ContactsAPI'
 
 class App extends Component {
+  state = {
+    contacts: []
+  }
+
+  componentDidMount() {
+    ContactsAPI.getAll().then((contacts) => {
+      this.setState({contacts})
+    })
+  }
+
+  removeContact = (contact) => {
+    this.setState((state) => ({
+      contacts: state.contacts.filter((c) => c.id !== contact.id)
+    }))
+
+    ContactsAPI.remove(contact)
+  }
+
   render() {
     return (
       <div>
-        Hello World
+        <ListContacts onDeleteContact={this.removeContact} contacts={this.state.contacts}/>
       </div>
     )
   }
 }
 
-export default App;
+export default App
